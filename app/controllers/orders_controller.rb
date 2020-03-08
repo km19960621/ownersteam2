b18249072a06ee8d2c26176bbaf8722ab7b1d4d9
keimurakami <k.m.1996.0621@gmail.com>
2020-03-08 21:00:24 +0900

    fix views of order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,16 @@ class OrdersController < ApplicationController
   before_action :login_required, except: [:index]
 
   def index
-    
+    @cards = Card.all
+    @pitchers = @cards.where(pitcher: "true")
+    @catchers = @cards.where(catcher: "true")
+    @firsts = @cards.where(first: "true")
+    @seconds = @cards.where(second: "true")
+    @thirds = @cards.where(third: "true")
+    @shorts = @cards.where(short: "true")
+    @lefts = @cards.where(left: "true")
+    @centers = @cards.where(center: "true")
+    @rights = @cards.where(right: "true")
+    @dhs = @cards.where(dh: "true")
   end
 end
