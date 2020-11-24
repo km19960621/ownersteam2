b182cb05599a85912e5a4761dd86f1bb679d52f5
keimurakami <k.m.1996.0621@gmail.com>
2020-11-24 22:56:41 +0900

    add yearly order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,96 @@ class UsersController < ApplicationController
     @dhs = @cards.where(dh: "true")
   end
 
+  def orders_2010
+    @user = User.find(params[:id])
+    @cards = @user.cards.where("image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ?", "OL01%", "OL02%", "OL03%", "OL04%")
+    @pitchers = @cards.where(pitcher: "true")
+    @catchers = @cards.where(catcher: "true")
+    @firsts = @cards.where(first: "true")
+    @seconds = @cards.where(second: "true")
+    @thirds = @cards.where(third: "true")
+    @shorts = @cards.where(short: "true")
+    @lefts = @cards.where(left: "true")
+    @centers = @cards.where(center: "true")
+    @rights = @cards.where(right: "true")
+    @dhs = @cards.where(dh: "true")
+  end
+
+  def orders_2011
+    @user = User.find(params[:id])
+    @cards = @user.cards.where("image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ?", "OL05%", "OL06%", "OL07%", "OL08%", "OLP01%", "OLP02%", "OLP03%")
+    @pitchers = @cards.where(pitcher: "true")
+    @catchers = @cards.where(catcher: "true")
+    @firsts = @cards.where(first: "true")
+    @seconds = @cards.where(second: "true")
+    @thirds = @cards.where(third: "true")
+    @shorts = @cards.where(short: "true")
+    @lefts = @cards.where(left: "true")
+    @centers = @cards.where(center: "true")
+    @rights = @cards.where(right: "true")
+    @dhs = @cards.where(dh: "true")
+  end
+
+  def orders_2012
+    @user = User.find(params[:id])
+    @cards = @user.cards.where("image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ?", "OL09%", "OL10%", "OL11%", "OL12%", "OLM01%", "OLP03%", "OLP04%", "OLP05%", "OLP06%", "OLP07%", "OLP08%", "OLP09%", "OLB01%", "OLB02%", "OLB03%", "OLB04%", "OLB05%")
+    @pitchers = @cards.where(pitcher: "true")
+    @catchers = @cards.where(catcher: "true")
+    @firsts = @cards.where(first: "true")
+    @seconds = @cards.where(second: "true")
+    @thirds = @cards.where(third: "true")
+    @shorts = @cards.where(short: "true")
+    @lefts = @cards.where(left: "true")
+    @centers = @cards.where(center: "true")
+    @rights = @cards.where(right: "true")
+    @dhs = @cards.where(dh: "true")
+  end
+
+  def orders_2013
+    @user = User.find(params[:id])
+    @cards = @user.cards.where("image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ?", "OL13%", "OL14%", "OL15%", "OL16%", "OLM02%", "OLP10%", "OLP11%", "OLP12%", "OLP13%", "OLP14%", "OLP15%", "OLP16%", "OLP17%", "OLP18%", "OLB06%", "OLB07%", "OLB08%", "OLB09%", "OLB10%", "OLB11%", "OLE01%")
+    @pitchers = @cards.where(pitcher: "true")
+    @catchers = @cards.where(catcher: "true")
+    @firsts = @cards.where(first: "true")
+    @seconds = @cards.where(second: "true")
+    @thirds = @cards.where(third: "true")
+    @shorts = @cards.where(short: "true")
+    @lefts = @cards.where(left: "true")
+    @centers = @cards.where(center: "true")
+    @rights = @cards.where(right: "true")
+    @dhs = @cards.where(dh: "true")
+  end
+
+  def orders_2014
+    @user = User.find(params[:id])
+    @cards = @user.cards.where("image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ?", "OL17%", "OL18%", "OL19%", "OL20%", "OLM03%", "OLP19%", "OLP20%", "OLP21%", "OLP22%", "OLP23%", "OLP24%", "OLP25%", "OLB12%", "OLB13%", "OLB14%", "OLB15%", "OLE01%", "OLE02%", "OLE03%")
+    @pitchers = @cards.where(pitcher: "true")
+    @catchers = @cards.where(catcher: "true")
+    @firsts = @cards.where(first: "true")
+    @seconds = @cards.where(second: "true")
+    @thirds = @cards.where(third: "true")
+    @shorts = @cards.where(short: "true")
+    @lefts = @cards.where(left: "true")
+    @centers = @cards.where(center: "true")
+    @rights = @cards.where(right: "true")
+    @dhs = @cards.where(dh: "true")
+  end
+
+  def orders_2015
+    @user = User.find(params[:id])
+    @cards = @user.cards.where("image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ? or image LIKE ?", "OL21%", "OL22%", "OL23%", "OL24%", "OLS01%", "OLP26%", "OLP27%", "OLP28%", "OLE04%")
+    @pitchers = @cards.where(pitcher: "true")
+    @catchers = @cards.where(catcher: "true")
+    @firsts = @cards.where(first: "true")
+    @seconds = @cards.where(second: "true")
+    @thirds = @cards.where(third: "true")
+    @shorts = @cards.where(short: "true")
+    @lefts = @cards.where(left: "true")
+    @centers = @cards.where(center: "true")
+    @rights = @cards.where(right: "true")
+    @dhs = @cards.where(dh: "true")
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :image, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,14 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   resources :users
-  get 'users/:id/orders', to: 'users#orders'
   get 'users/:id/cards', to: 'users#cards'
+  get 'users/:id/orders', to: 'users#orders'
+  get 'users/:id/orders-2010', to: 'users#orders_2010'
+  get 'users/:id/orders-2011', to: 'users#orders_2011'
+  get 'users/:id/orders-2012', to: 'users#orders_2012'
+  get 'users/:id/orders-2013', to: 'users#orders_2013'
+  get 'users/:id/orders-2014', to: 'users#orders_2014'
+  get 'users/:id/orders-2015', to: 'users#orders_2015'
 
   resources :cards
 
